904d691b4ae9ceda471dd1e5ae3d21893aefcd03
azne <azne.64069@gmail.com>
2023-05-22 23:10:25 +0900

    とりあえず見た目だけ作った

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -8,13 +8,49 @@ const App = () => {
   return (
     <ThemeProvider>
       <div className={styles.root}>
-        <header className={styles.header}>header</header>
+        <header className={styles.header.wrapper}>
+          <h1 className={styles.header.title}>training-fe ToDoApp</h1>
+          <div>
+            <button className={styles.menuBtn}>
+              <svg width="5" height="21" viewBox="0 0 5 21" fill="none" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" clipRule="evenodd" d="M4.375 2.16663C4.375 3.20216 3.53553 4.04163 2.5 4.04163C1.46447 4.04163 0.625 3.20216 0.625 2.16663C0.625 1.13109 1.46447 0.291626 2.5 0.291626C3.53553 0.291626 4.375 1.13109 4.375 2.16663ZM4.375 10.5C4.375 11.5355 3.53553 12.375 2.5 12.375C1.46447 12.375 0.625 11.5355 0.625 10.5C0.625 9.46443 1.46447 8.62496 2.5 8.62496C3.53553 8.62496 4.375 9.46443 4.375 10.5ZM2.5 20.7083C3.53553 20.7083 4.375 19.8688 4.375 18.8333C4.375 17.7978 3.53553 16.9583 2.5 16.9583C1.46447 16.9583 0.625 17.7978 0.625 18.8333C0.625 19.8688 1.46447 20.7083 2.5 20.7083Z" fill="#287FB8"/></svg>
+            </button>
+            <div className={styles.menu.wrapper}>
+              <label className={styles.menu.label}>
+                <input className={styles.menu.checkbox} type="checkbox" />
+                <span className={styles.menuCheckIcon}>check</span>
+                非表示タスクも表示
+              </label>
+            </div>
+          </div>
+        </header>
         <main className={styles.main}>
-          <section>
-            <h1>Hello Vite App</h1>
-          </section>
+          <div className={styles.todo.wrapper}>
+            <label className={styles.todo.label}>
+              <input className={styles.todoCheckbox} type="checkbox" />
+              <span className={styles.todoCheckIcon}>check</span>
+            </label>
+            <div className={styles.todo.contents}>
+              <div className={styles.todo.taskName}>タスクの表示タスクの表示</div>
+              <input className={styles.todo.taskNameInput} type="text" name="name" placeholder="タスクを入力..." autoComplete="off" maxLength={128}></input>
+              <div className={styles.todo.taskText}>説明文が入る</div>
+              <input className={styles.todo.taskTextInput} type="text" name="name" placeholder="説明文を入力..." autoComplete="off" maxLength={128}></input>
+            </div>
+          </div>
+          <div className={styles.todo.wrapper}>
+            <label className={styles.todo.label}>
+              <input className={styles.todoCheckbox} type="checkbox" />
+              <span className={styles.todoCheckIcon}>check</span>
+            </label>
+            <div className={styles.todo.contents}>
+              <div className={styles.todo.taskName}>タスクの表示タスク</div>
+              <div className={styles.todo.taskText}>説明文が入る</div>
+            </div>
+          </div>
+
         </main>
-        <footer className={styles.footer}>footer</footer>
+        <footer className={styles.footer}>
+          <button className={styles.addTodoBtn}><span className={styles.addTodoBtnIcon}></span>新規</button>
+        </footer>
       </div>
     </ThemeProvider>
   );
--- src/app/styles.css.ts
+++ src/app/styles.css.ts
@@ -1,24 +1,52 @@
-import { createVar, style } from "@vanilla-extract/css";
+import { createVar, style, globalStyle } from "@vanilla-extract/css";
 import { calc } from "@vanilla-extract/css-utils";
 
 const headerSize = createVar();
 const footerSize = createVar();
+const mainColor = createVar();
+const textColor = createVar();
+const subColor = createVar();
+const bgColor = createVar();
+
+globalStyle("body", {
+  backgroundColor: "#EFEFEF",
+});
 
 export const root = style({
   vars: {
     [headerSize]: "64px",
-    [footerSize]: "128px",
+    [footerSize]: "64px",
+    [mainColor]: "#287FB8",
+    [textColor]: "#2D2D2D",
+    [subColor]: "#95A6A7",
+    [bgColor]: "#EFEFEF",
   },
+  minWidth: "320px",
+  maxWidth: "520px",
+  margin: "0 auto",
+  color: textColor,
+  fontSize: "16px",
 });
 
-export const header = style({
-  height: headerSize,
-  display: "flex",
-  alignItems: "center",
-});
+export const header = {
+  wrapper: style({
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "relative",
+    height: headerSize,
+    padding: "0 1em",
+  }),
+  title: style({
+    marginBottom: "1em",
+    fontSize: "0.75rem",
+    fontWeight: "normal",
+  }),
+};
 
 export const main = style({
   minHeight: calc.subtract("100vh", headerSize, footerSize),
+  padding: "0 1em",
   "@supports": {
     "(min-height: 100dvh)": {
       minHeight: calc.subtract("100dvh", headerSize, footerSize),
@@ -26,8 +54,236 @@ export const main = style({
   },
 });
 
+export const menuBtn = style({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "32px",
+  height: "32px",
+  border: `2px solid ${mainColor}`,
+  borderRadius: "50%",
+  backgroundColor: "transparent",
+});
+
+export const menu = {
+  wrapper: style({
+    position: "absolute",
+    top: "3.5em",
+    right: "1em",
+    minWidth: "160px",
+    padding: "0.5em 1.25em",
+    borderRadius: "0.5rem",
+    backgroundColor: "#FFFFFF",
+    boxShadow: "0 4px 4px rgba(0, 0, 0, 0.2)",
+  }),
+  label: style({
+    display: "block",
+    padding: "0.5em 0",
+    fontSize: "0.75rem",
+  }),
+  checkbox: style({
+    appearance: "none",
+    opacity: 0,
+    position: "absolute",
+  }),
+};
+
+export const menuCheckIcon = style({
+  display: "inline-block",
+  overflow: "hidden",
+  position: "relative",
+  boxSizing: "border-box",
+  height: "1.5em",
+  width: "1.5em",
+  marginRight: "0.5em",
+  marginTop: "-0.25em",
+  verticalAlign: "middle",
+  textIndent: "200%",
+  whiteSpace: "nowrap",
+  transition: "all .2s ease-out",
+
+  ":before": {
+    content: "''",
+    position: "absolute",
+    top: 0,
+    left: 0,
+    width: "100%",
+    height: "100%",
+    border: "none",
+    backgroundColor: bgColor,
+    borderRadius: "0.25rem",
+    boxSizing: "border-box",
+  },
+  ":after": {
+    content: "''",
+    position: "absolute",
+    top: "42%",
+    left: "50%",
+    width: "0.5em",
+    height: "0.75em",
+    border: `solid ${mainColor}`,
+    borderWidth: "0 0.125em 0.125em 0",
+    transform: "translate(-50%, -50%) rotate(45deg)",
+    opacity: 0,
+  },
+  selectors: {
+    [`${menu.checkbox}:checked + &:after`]: {
+      opacity: 1,
+    },
+  },
+});
+
+export const todo = {
+  wrapper: style({
+    display: "flex",
+    marginBottom: "1em",
+  }),
+  label: style({
+    fontSize: "1rem",
+  }),
+  contents: style({
+    flex: 1,
+    lineHeight: 1,
+  }),
+  taskName: style({
+    marginBottom: "0.5em",
+    lineHeight: 1.4,
+    fontSize: "1rem",
+    cursor: "pointer",
+  }),
+  taskText: style({
+    fontSize: "0.75rem",
+    color: subColor,
+    cursor: "pointer",
+  }),
+  taskNameInput: style({
+    width: "100%",
+    marginBottom: "0.5em",
+    lineHeight: 1.4,
+    borderRadius: "0.25rem",
+    border: "none",
+    backgroundColor: "transparent",
+    fontSize: "1rem",
+    color: textColor,
+    ":focus": {
+      outline:`2px solid ${mainColor}`,
+    },
+  }),
+  taskTextInput: style({
+    width: "100%",
+    borderRadius: "0.25rem",
+    border: "none",
+    backgroundColor: "transparent",
+    fontSize: "0.75rem",
+    color: subColor,
+    ":focus": {
+      outline:`2px solid ${mainColor}`,
+    },
+  }),
+};
+
+export const todoCheckbox = style({
+  selectors: {
+    [`${todo.label} &`]: {
+      appearance: "none",
+      opacity: 0,
+      position: "absolute",
+    },
+  },
+});
+
+export const todoCheckIcon = style({
+  selectors: {
+    [`${todo.label} &`]: {
+      display: "inline-block",
+      overflow: "hidden",
+      position: "relative",
+      boxSizing: "border-box",
+      height: "1.5em",
+      width: "1.5em",
+      marginRight: "0.5em",
+      marginTop: "-0.25em",
+      verticalAlign: "middle",
+      textIndent: "200%",
+      whiteSpace: "nowrap",
+      transition: "all .2s ease-out",
+    },
+    [`${todo.label} &:before`]: {
+      content: "''",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      width: "100%",
+      height: "100%",
+      border: `1px solid ${subColor}`,
+      borderRadius: "0.25rem",
+      boxSizing: "border-box",
+    },
+    [`${todo.label} &:after`]: {
+      content: "''",
+      position: "absolute",
+      top: "42%",
+      left: "50%",
+      width: "0.5em",
+      height: "0.75em",
+      border: `solid ${mainColor}`,
+      borderWidth: "0 0.125em 0.125em 0",
+      transform: "translate(-50%, -50%) rotate(45deg)",
+      opacity: 0,
+    },
+    [`${todoCheckbox}:checked + &:after`]: {
+      opacity: 1,
+    },
+  },
+});
+
+export const addTodoBtn = style({
+  display: "flex",
+  alignItems: "center",
+  height: "2.5em",
+  border: "none",
+  backgroundColor: "transparent",
+  appearance: "none",
+  fontSize: "1.25rem",
+  color: mainColor,
+});
+
+export const addTodoBtnIcon = style({
+  display: "inline-block",
+  position: "relative",
+  width: "24px",
+  height: "24px",
+  marginRight: "0.5em",
+  backgroundColor: mainColor,
+  borderRadius: "50%",
+
+  ":before": {
+    content: "''",
+    display: "block",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "16px",
+    height: "1px",
+    backgroundColor: "#FFFFFF",
+  },
+  ":after": {
+    content: "''",
+    display: "block",
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%,-50%)",
+    width: "1px",
+    height: "16px",
+    backgroundColor: "#FFFFFF",
+  },
+});
+
 export const footer = style({
   height: footerSize,
   display: "flex",
   alignItems: "center",
+  padding: "0 1em",
 });
